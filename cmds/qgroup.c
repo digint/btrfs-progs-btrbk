06863cf5abace0003f4c959117affbf28c5e5086
Axel Burri <axel@tty0.ch>
2018-09-12 15:14:15 +0200

    btrfs-progs: remove unneeded dependencies on separated build (-DBTRFS_SEPARATED_BUILD)
    Remove references to unneeded symbols when building separated
    subcommands: "btrfs-*.separated.o".
    
    Note that this patch still leaves unreferenced static symbols, which
    in turn makes the compiler warn about "unused function/variable".
    Stripping all symbols would imply adaptions in the source code
    (especially by moving "int handle_command_group" functionality into a
    separate compile unit).
    
    This is not a problem, ignore these warnings when building object:
    
        $(CC) -Wno-unused-function -Wno-unused-const-variable

--- cmds/qgroup.c
+++ cmds/qgroup.c
@@ -2146,6 +2146,7 @@ static DEFINE_SIMPLE_COMMAND(qgroup_clear_stale, "clear-stale");
 static const char qgroup_cmd_group_info[] =
 "manage quota groups";
 
+#ifndef BTRFS_SEPARATED_BUILD
 static const struct cmd_group qgroup_cmd_group = {
 	qgroup_cmd_group_usage, qgroup_cmd_group_info, {
 		&cmd_struct_qgroup_assign,
@@ -2160,3 +2161,4 @@ static const struct cmd_group qgroup_cmd_group = {
 };
 
 DEFINE_GROUP_COMMAND_TOKEN(qgroup);
+#endif
